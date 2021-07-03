00d98da1b06d0fc6d0d2fffe117bf9aa099eda19
Akash Devgan <akash.devgan@gmail.com>
2021-07-02 23:14:50 -0400

    Day 08: Create a method to load and parse sprite sheet

--- 06-dino/dino.py
+++ 06-dino/dino.py
@@ -1,33 +1,4 @@
-import enum
-
-import pygame.transform
-
 from engine.main import *
-import sys
-
-
-class Sprite:
-    def __init__(self, image, frame_size, total_frames):
-        self.total_frames = total_frames
-        self.frame_size = frame_size
-        self.image = image
-        self.frame = 0
-        self.frame_time = 200
-        self.last_update_time = 0
-
-    def update(self, game_time):
-        if game_time - self.last_update_time >= self.frame_time:
-            self.last_update_time = game_time
-            self.frame += 1
-            self.frame %= self.total_frames
-
-    def render(self, screen):
-        screen.blit(self.image, (0, 0), (self.frame * self.frame_size.x, 0, self.frame_size.x, self.frame_size.y))
-
-
-class SpriteSheet:
-    def __init__(self, total_animations):
-        self.total_animations = total_animations
 
 
 class DinoGame(Game):
@@ -38,55 +9,18 @@ class DinoGame(Game):
         self.dino_sprites = []
 
     def load_assets(self):
-        dino_scale = 3
-        dino_frame_width = 24
-        dino_frame_height = 24
-        dino_image = pygame.image.load('./img/DinoSprites - vita.png').convert_alpha()
-        dino_rect = dino_image.get_rect()
-        dino_image = pygame.transform.scale(dino_image, (dino_rect.width * dino_scale, dino_rect.height * dino_scale))
+        self.dino_sprites = self.get_sprites(
+            './img/DinoSprites - vita.png',
+            pygame.Vector2(24, 24),
+            [4, 6, 3, 4, 7],
+            3
+        )
 
-        # Idle
-        idle_frames = 4
-        image = pygame.Surface((dino_frame_width * dino_scale * idle_frames, dino_frame_height * dino_scale)).convert_alpha()
-        image.blit(dino_image, (0, 0), (0, 0, dino_frame_width * dino_scale * idle_frames, dino_frame_height * dino_scale))
-        image.set_colorkey((0, 0, 0))
-        self.dino_sprites.append(
-            Sprite(image, pygame.Vector2(dino_frame_width * dino_scale, dino_frame_height * dino_scale), 4))
-
-        # Move
-        move_frames = 6
-        image = pygame.Surface((dino_frame_width * dino_scale * move_frames, dino_frame_height * dino_scale)).convert_alpha()
-        image.blit(dino_image, (0, 0), (24 * 3 * (idle_frames), 0, dino_frame_width * dino_scale * 6, dino_frame_height * dino_scale))
-        image.set_colorkey((0, 0, 0))
-        self.dino_sprites.append(
-            Sprite(image, pygame.Vector2(dino_frame_width * dino_scale, dino_frame_height * dino_scale), move_frames))
-
-        # Kick
-        kick_frames = 3
-        image = pygame.Surface((dino_frame_width * dino_scale * kick_frames, dino_frame_height * dino_scale)).convert_alpha()
-        image.blit(dino_image, (0, 0), (24 * 3 * (idle_frames + move_frames), 0, dino_frame_width * dino_scale * kick_frames, dino_frame_height * dino_scale))
-        image.set_colorkey((0, 0, 0))
-        self.dino_sprites.append(
-            Sprite(image, pygame.Vector2(dino_frame_width * dino_scale, dino_frame_height * dino_scale), kick_frames))
-
-        # Hurt
-        hurt_frames = 4
-        image = pygame.Surface((dino_frame_width * dino_scale * hurt_frames, dino_frame_height * dino_scale)).convert_alpha()
-        image.blit(dino_image, (0, 0), (24 * 3 * (idle_frames + move_frames + kick_frames), 0, dino_frame_width * dino_scale * hurt_frames, dino_frame_height * dino_scale))
-        image.set_colorkey((0, 0, 0))
-        self.dino_sprites.append(
-            Sprite(image, pygame.Vector2(dino_frame_width * dino_scale, dino_frame_height * dino_scale), hurt_frames))
-
-        # Run
-        run_frames = 7
-        image = pygame.Surface((dino_frame_width * dino_scale * run_frames, dino_frame_height * dino_scale)).convert_alpha()
-        image.blit(dino_image, (0, 0), (24 * 3 * (idle_frames + move_frames + kick_frames + hurt_frames), 0, dino_frame_width * dino_scale * run_frames, dino_frame_height * dino_scale))
-        image.set_colorkey((0, 0, 0))
-        self.dino_sprites.append(
-            Sprite(image, pygame.Vector2(dino_frame_width * dino_scale, dino_frame_height * dino_scale), run_frames))
-
-    def update(self, game_time):
-        for event in pygame.event.get():
+    def update(self, events, game_time):
+        for event in events:
+            if event.type == pygame.QUIT:
+                self.running = False
+                return
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
                     self.dino_action -= 1
@@ -96,7 +30,6 @@ class DinoGame(Game):
                     self.dino_action += 1
                     self.dino_action %= len(self.dino_sprites)
                     self.dino_sprites[self.dino_action].frame = 0
-
         self.dino_sprites[self.dino_action].update(game_time)
 
     def render(self):
@@ -104,6 +37,5 @@ class DinoGame(Game):
         self.dino_sprites[self.dino_action].render(self.screen)
 
 
-game = DinoGame(600, 400, 'Same', 60)
+game = DinoGame(600, 400, 'Dino', 60)
 game.run()
-sys.exit()
--- engine/main.py
+++ engine/main.py
@@ -1,3 +1,5 @@
+import sys
+
 import pygame
 
 
@@ -22,25 +24,64 @@ class Game:
     def run(self):
         self.initialize()
         while self.running:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    self.running = False
-
-            self.update(pygame.time.get_ticks())
+            self.update(pygame.event.get(), pygame.time.get_ticks())
             self.render()
             pygame.display.update()
             self.clock.tick(self.fps)
 
         self.shutdown()
 
-    def update(self, game_time):
-        pass
+    def update(self, events, game_time):
+        for event in events:
+            if event.type == pygame.QUIT:
+                self.running = False
 
     def render(self):
         pass
 
     def shutdown(self):
         pygame.quit()
+        sys.exit()
 
     def load_assets(self):
         pass
+
+    def get_sprites(self, image_path, frame_size, animation_steps, scale=1):
+        frame = pygame.Vector2(frame_size * scale, frame_size * scale)
+
+        sprite_sheet = pygame.image.load(image_path).convert_alpha()
+        rect = sprite_sheet.get_rect()
+        sprite_sheet = pygame.transform.scale(sprite_sheet, (rect.width * scale, rect.height * scale))
+        cursor = 0
+        sprites = []
+        for steps in animation_steps:
+            image = pygame.Surface((frame.x * steps, frame.y)).convert_alpha()
+            image.blit(
+                sprite_sheet,
+                (0, 0),
+                pygame.Rect(frame.x * cursor, 0, frame.x * steps, frame.y)
+            )
+            image.set_colorkey((0, 0, 0))
+            sprites.append(Sprite(image, pygame.Vector2(frame.x, frame.y), steps))
+            cursor += steps
+
+        return sprites
+
+
+class Sprite:
+    def __init__(self, image, frame_size, total_frames):
+        self.total_frames = total_frames
+        self.frame_size = frame_size
+        self.image = image
+        self.frame = 0
+        self.frame_time = 200
+        self.last_update_time = 0
+
+    def update(self, game_time):
+        if game_time - self.last_update_time >= self.frame_time:
+            self.last_update_time = game_time
+            self.frame += 1
+            self.frame %= self.total_frames
+
+    def render(self, screen):
+        screen.blit(self.image, (0, 0), (self.frame * self.frame_size.x, 0, self.frame_size.x, self.frame_size.y))
